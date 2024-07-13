e347b3a6858b5040d008ad765f25c3d6f76941ea
Ivan <ivan-kozlow04@mail.ru>
2024-07-13 13:55:11 +0300

    fix redirect on auth layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,10 +1,13 @@
 import { PropsWithChildren } from 'react'
 import { redirect } from 'next/navigation'
 
+import { getServerAuth } from '@/utils/getServerAuth/getServerAuth'
+
 type TypeProps = PropsWithChildren
 
 export async function Layout({ children }: TypeProps) {
 	const user = await getServerAuth()
-	if (user?.isLoggedIn) return redirect(user.isAdmin && '/')
+
+	if (user?.isLoggedIn) return redirect(user.isAdmin ? '/' : '/')
 	return children
 }
